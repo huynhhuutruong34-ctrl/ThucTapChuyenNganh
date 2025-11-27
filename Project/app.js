2010f999f0883ad2565dc91486d7110d402dea27
HuuTruong <huynhhuutruong34@gmail.com>
2025-11-27 07:48:12 +0700

    Login

--- Project/app.js
+++ Project/app.js
@@ -39,11 +39,7 @@ app.use('/users', usersRouter);
 //app.use('/admin/test', testRouter);
 
 
-// catch 404 and forward to error handler
-app.use(function(req,
-                 res, next) {
-  next(createError(404));
-});
+
 
 // error handler
 app.use(function(err, req, res, next) {
@@ -56,4 +52,59 @@ app.use(function(err, req, res, next) {
   res.render('error');
 });
 
+//database connect
+
+const mongoose = require('mongoose');
+const bcryptjs = require('bcryptjs');
+const User = require('./models/User');
+const bodyParser = require('body-parser');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+mongoose.connect("mongodb://127.0.0.1/node")
+    .then(()=> {
+        console.log("MongoDB Connected successfully.   ");
+    })
+    .catch((err)=> {
+        console.error("MongoDB Connected failed " , err);
+    });
+
+app.post('/login', (req, res) => {
+    User.findOne({email: req.body.email}).then((user) => {
+        if (user) {
+            bcryptjs.compare(req.body.password,user.password,(err,matched)=>{
+                if(err) return err;
+                if(matched){
+                    res.send("User was logged in");
+                }else {
+                    res.send("User was not logged in");
+                }
+            })
+        }
+    })
+});
+app.post('/register',  (req,res) => {
+    console.log(req.body);
+    const newUser = new User();
+        newUser.email = req.body.email;
+        newUser.password = req.body.password;
+        bcryptjs.genSalt(10, function (err, salt) {
+            bcryptjs.hash(newUser.password, salt, function (err, hash) {
+                if (err) {return  err}
+                newUser.password = hash;
+
+                newUser.save().then(userSave=>
+                {
+                    res.send('USER SAVED');
+                }).catch(err => {
+                    res.send('USER ERROR'+err);
+                });
+            });
+        });
+    }
+);
+// catch 404 and forward to error handler
+app.use(function(req,
+                 res, next) {
+    next(createError(404));
+});
 module.exports = app;
